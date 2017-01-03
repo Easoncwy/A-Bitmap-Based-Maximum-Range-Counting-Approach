14846c60f6f08328d13890b68df009f6548e4a99
Supreme <supreme.cwy@gmail.com>
2017-01-03 22:45:27 +0800

    Add a CompBitMapOps class in Method package.

--- src/Delete/Delete.java
+++ src/Delete/Delete.java
@@ -3,6 +3,9 @@ package Delete;
 import Entity.Interval;
 import Entity.Unit;
 import Entity.User;
+import Insert.Insert;
+import Method.CompBitMapOps;
+import Method.Time;
 import Test.PrintTestCases;
 
 import java.util.ArrayList;
@@ -18,7 +21,10 @@ public class Delete {
            ArrayList<String> allTimeArray,
            HashSet<String> allStartTimeSet,
            ArrayList<String> allUsers,
-           Map<String, ArrayList<Unit>> compressedMap){
+           Map<String, ArrayList<Unit>> compressedMap)throws Exception{
+
+        Time TIME = new Time();
+        CompBitMapOps cbmo = new CompBitMapOps();
 
         String start,end;
         int index = -1;
@@ -55,7 +61,6 @@ public class Delete {
         ptc.printUserIntervals(markUserMap);
 
 
-
         //如果删除这一区间后,该 user 就没有时间区间了
 
         if (user.isEmpty()){
@@ -84,9 +89,63 @@ public class Delete {
         }else {
             //如果删除这一区间后,该 user 还有时间区间
 
-            //判断 deleteI 的 开始时间点 和 结束时间点还存不存在
+            long startTime = TIME.uniformTime(start);
+            long endTime =  TIME.uniformTime(end);
 
-            
+            //删除这一区间后, 判断 deleteI 的 开始时间点 和 结束时间点还存不存在
+            boolean havaStart = false;
+            boolean haveEnd = false;
+
+            for (Integer order: markUserMap.keySet()) {
+                User u = markUserMap.get(order);
+                ArrayList<Interval> intervals = u.intervals;
+                for (int i = 0; i < intervals.size(); i++){
+                    if (intervals.get(i).start.equals(start))
+                        havaStart = true;
+                    if (intervals.get(i).end.equals(end))
+                        haveEnd = true;
+                    if (havaStart && haveEnd)
+                        break;
+                }
+            }
+
+            ++index;//由于用户序号是从0开始计数.为了setOne方法 计数方便,需要把 index 加一
+
+
+            // 将allTimeArray中属于(start, end)范围中的时间点, 做setOne操作.
+            for (int i = 0; i < allTimeArray.size(); i++) {
+                String time = allTimeArray.get(i);
+                long t = TIME.uniformTime(time);
+                if ((t > startTime) && (t < endTime)){
+
+                    //将属于(start, end)范围中的时间点做setOne操作.
+                    ArrayList<Unit> cbitmap = compressedMap.get(time);
+                    ArrayList<Unit> newCBitMap = cbmo.
+                    compressedMap.put(time, newCBitMap);
+
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/Method/CompBitMapOps.java
@@ -0,0 +1,140 @@
+package Method;
+
+import Entity.Unit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eason on 2017/1/3.
+ */
+public class CompBitMapOps {
+
+
+
+    public ArrayList<Unit> setZero(int index, ArrayList<Unit> cbitmap){
+
+
+
+
+
+
+    }
+
+
+    /**
+     *
+     * setOne
+     *
+     * @param index
+     * @param cbitmap
+     * @return
+     */
+    public ArrayList<Unit> setOne(int index, ArrayList<Unit> cbitmap){
+        int i,count=0,j;
+        ArrayList<Unit> newCBitMap = new ArrayList<>();
+        for( i = 0; i < cbitmap.size(); i++){
+            Unit unit = cbitmap.get(i);
+            count =unit.count;
+            if(index > count){
+                newCBitMap.add(unit);
+                index = index - count;
+            }else{
+                if(index == 1){
+                    Unit lastUnit = newCBitMap.get(newCBitMap.size()-1);
+                    lastUnit.count++;
+                    if (count -1 > 0){
+                        Unit u2 = new Unit(count-1,0);
+                        newCBitMap.add(u2);
+                        break;
+                    }
+
+                }else if(index == count){
+                    Unit u1 = new Unit(count-1,0);
+                    newCBitMap.add(u1);
+
+                    if( i == (cbitmap.size()-1)){
+                        newCBitMap.add(new Unit(1,1));
+                    }else{
+                        Unit u3 = cbitmap.get(i+1);
+                        u3.count++;
+                    }
+                    break;
+                }else{
+                    Unit u1 = new Unit(index-1,0);
+                    Unit u2 = new Unit(1,1);
+                    Unit u3 = new Unit(count - index, 0);
+                    newCBitMap.add(u1);
+                    newCBitMap.add(u2);
+                    newCBitMap.add(u3);
+                    break;
+                }
+            }
+        }
+
+
+
+        for(j = i+1; j < cbitmap.size(); j++){
+            newCBitMap.add(cbitmap.get(j));
+        }
+        return newCBitMap;
+    }
+
+    /**
+     * addOne
+     *
+     * @param index
+     * @param cbitmap
+     * @return
+     */
+    public ArrayList<Unit> addOne(int index, ArrayList<Unit> cbitmap){
+
+        ArrayList<Unit> newCBitMap = new ArrayList<>();
+
+        int size = cbitmap.size();
+        for (int i = 0; i < size - 1; i++) {
+            Unit unit = cbitmap.get(i);
+            newCBitMap.add(unit);
+        }
+        Unit lastUnit = cbitmap.get(size - 1);
+        if (lastUnit.bit == 1){
+            ++lastUnit.count;
+            newCBitMap.add(lastUnit);
+
+        }else {
+            newCBitMap.add(lastUnit);
+            Unit unit = new Unit(1,1);
+            newCBitMap.add(unit);
+        }
+        return newCBitMap;
+    }
+
+    /**
+     * addZero
+     *
+     * @param index
+     * @param cbitmap
+     * @return
+     */
+    public ArrayList<Unit> addZero(int index, ArrayList<Unit> cbitmap){
+        ArrayList<Unit> newCBitMap = new ArrayList<>();
+
+        int size = cbitmap.size();
+        for (int i = 0; i < size - 1; i++) {
+            Unit unit = cbitmap.get(i);
+            newCBitMap.add(unit);
+        }
+
+        Unit lastUnit = cbitmap.get(size - 1);
+        if (lastUnit.bit == 0){
+            ++lastUnit.count;
+            newCBitMap.add(lastUnit);
+
+        }else {
+            newCBitMap.add(lastUnit);
+            Unit unit = new Unit(1,0);
+            newCBitMap.add(unit);
+        }
+
+        return newCBitMap;
+    }
+}
